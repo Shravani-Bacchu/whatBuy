11e96cbc9b535549b69b9d8fd71c847e428441e3
Mia Chiang <mialchiang@gmail.com>
2024-08-13 11:37:27 +0100

    Initial Commit

---
+++ whatBuy/whatBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatBuyApp.swift
+//  whatBuy
+//
+//  Created by Scholar on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct whatBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
