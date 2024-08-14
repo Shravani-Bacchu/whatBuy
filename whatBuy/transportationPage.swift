128bd915e7826cace6ac3d4f7b031b038cf06c80
Shravani <shravanibacchu@gmail.com>
2024-08-14 15:43:46 +0100

    transportation

--- whatBuy/transportationPage.swift
+++ whatBuy/transportationPage.swift
@@ -9,23 +9,33 @@ import SwiftUI
 
 struct transportationPage: View {
     var body: some View {
-        Text("Transportation Methods")
-            .font(.title)
-            .fontWeight(.bold)
-            .padding()
-        HStack{
-            Text("    Tube")
-                .font(.title)
-            Spacer()
+        ZStack{
+            Color(.customBrown)
+                .ignoresSafeArea()
+            VStack{
+                Text("Transportation Methods")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("Tips and Tricks to travel more sustainably")
+                    .font(.title3)
+                Text("Tube")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                Text(" • You can use a debit or a credit card to tap in and out")
+                Text("•Some notable stations in London are Paddington,King's Cross - St Pancras and Victoria")
+                Text("")
+                Text("Cycle")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                Text("• There are many opportunities to hire bikes in central london")
+                Text(" • Santander bikes and lime bikes are potential ways to hire a bike.")
+                Spacer()
+                HStack{
+                }
+            }//vstackcloses
         }
-        Spacer()
-        
-        Text("Bus")
-            .font(.title)
-        Text("Cycle")
-            .font(.title)
-    }
-}
+    }//closesbody
+}//structclosing
     #Preview {
         transportationPage()
     }
